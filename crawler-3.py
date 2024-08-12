780dbd5114ffabd71c0bed1b6bb0d91cafcd60bf
AliceInPain <nazaninshafaei.ns@gmail.com>
2024-08-12 14:50:14 +0330

    added description and tables

--- crawler-3.py
+++ crawler-3.py
@@ -11,7 +11,8 @@ def fetch_data(url):
     links = []
     images = []
     prices = []
-    description = []
+    descriptions = []
+    tables = []
 
     # Get all the titles with h1
     title_tag = soup.find(["h1"])
@@ -29,8 +30,8 @@ def fetch_data(url):
     for tag in soup.find_all('p', class_="price"):
         prices.append(tag.get_text(strip=True))
 
-    page_description = {}
     # to get description
+    page_description = {}  # a dict to store our description with key and value
     div_tag = soup.find("div", id="tab-description")
     # print(div_tag)
     if div_tag:
@@ -38,22 +39,41 @@ def fetch_data(url):
         for index, h2 in enumerate(h2_tags, start=1):
             key = f"h2_{index}"
             page_description[key] = h2.get_text()
-            
+
         h3_tags = div_tag.find_all("h3")
         for index, h3 in enumerate(h3_tags, start=1):
             key = f"h3_{index}"
             page_description[key] = h3.get_text()
-            
-        # description[0] = page_description
-        description = [(k,v) for k,v in page_description.items()]
-        # print(description)
+        img_tags = div_tag.find_all("img")
+        for index, img in enumerate(img_tags, start=1):
+            key = f"img_{index}"
+            page_description[key] = img.get("src")
+        descriptions = [(k, v) for k, v in page_description.items()]
+        # print(descriptions)
+
+    # to get tables
+    page_table = {}
+    table_tag = soup.find("table", class_="shop_attributes")
+    if table_tag:
+        tr_tag = table_tag.find_all("tr")
+        # print(type(tr_tag)) #---> <class 'bs4.element.ResultSet'>
+        for tr in tr_tag:
+            th_tag = tr.find("th")
+            td_tag = tr.find("td")
+            if td_tag and td_tag:
+                key = th_tag.get_text(strip=True)
+                value = td_tag.get_text(strip=True)
+                page_table[key] = value
+    tables = [(k, v) for k, v in page_table.items()]
+    # pprint(tables)
 
     return {
         "title": titles,
         "image": images,
         "price": prices,
         "link": links,
-        "description": description
+        "description": descriptions,
+        "table": tables
     }
 
 
@@ -85,10 +105,12 @@ for website_name, attributes in data.items():
     images = attributes.get("image", [])
     prices = attributes.get("price", [])
     links = attributes.get("link", [])
+    descriptions = attributes.get("description", [])
+    tables = attributes.get("table", [])
 
    # maximum length of lists
     max_length = max(len(website_name), len(titles),
-                     len(images), len(prices), len(links))
+                     len(images), len(prices), len(links), len(descriptions), len(tables))
 
     # loop through inner lists
     for i in range(max_length):
@@ -97,7 +119,9 @@ for website_name, attributes in data.items():
             "Title": titles[i] if i < len(titles) else None,
             "Image": images[i] if i < len(images) else None,
             "Price": prices[i] if i < len(prices) else None,
-            "Link": links[i] if i < len(links) else None
+            "Link": links[i] if i < len(links) else None,
+            "Description": descriptions[i] if i < len(descriptions) else None,
+            "Table": tables[i] if i < len(tables) else None
         }
 
         rows.append(row)
